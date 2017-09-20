9c7538a04ad5c37186db98d4b6193ccea3a85268
llincc <llincc@126.com>
2017-09-20 15:55:34 +0800

    init project

--- app/src/main/java/com/example/linch/miniweather/MainActivity.java
+++ app/src/main/java/com/example/linch/miniweather/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.linch.miniweather;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by linch on 2017/9/20.
  */
 
-public class MainActivity {
-    
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState ){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
 }
