993da7f2d69e846c4324d6755795362986ab58f2
llincc <llincc@126.com>
2017-10-04 13:37:16 +0800

    Add network detection & class NetUtil

--- app/src/main/java/com/example/linch/miniweather/MainActivity.java
+++ app/src/main/java/com/example/linch/miniweather/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.linch.miniweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by linch on 2017/9/20.
@@ -12,5 +14,17 @@ public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState ){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        //网咯测试
+        if(NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
+            Log.d("myWeather","网络OK");
+
+            Toast.makeText(MainActivity.this,"网络OK！",Toast.LENGTH_LONG).show();
+        }
+        else{
+            Log.d("myWeather","网络炸了");
+
+            Toast.makeText(MainActivity.this,"网络炸了！",Toast.LENGTH_LONG).show();
+        }
     }
 }
