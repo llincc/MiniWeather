a99f309bfb75d9f2f148885941a53a1ec3684a5b
llincc <llincc@126.com>
2017-10-12 11:00:36 +0800

    Init view

--- app/src/main/java/com/example/linch/miniweather/MainActivity.java
+++ app/src/main/java/com/example/linch/miniweather/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -28,6 +29,9 @@ import java.net.URLEncoder;
 public class MainActivity extends Activity implements View.OnClickListener{
     private ImageView mUpdateBtn;
 
+    private TextView  cityTv, timeTv, humidityTv, weekTv, pmDataTv, pmQualityTv, temperatureTv,
+                       climateTv, windTv, city_name_Tv;
+    private ImageView weatherImg, pmImg;
     @Override
     protected void onCreate(Bundle savedInstanceState ){
         super.onCreate(savedInstanceState);
@@ -46,6 +50,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
             Toast.makeText(MainActivity.this,"网络炸了！",Toast.LENGTH_LONG).show();
         }
+
+        initView();
     }
     @Override
     public void onClick(View view){
@@ -66,6 +72,34 @@ public class MainActivity extends Activity implements View.OnClickListener{
         }
     }
 
+    /**
+     * 初始化天气信息
+     */
+    private  void initView(){
+        city_name_Tv = (TextView) findViewById(R.id.title_city_name);
+        cityTv = (TextView) findViewById(R.id.city);
+        timeTv = (TextView) findViewById(R.id.time);
+        humidityTv = (TextView) findViewById(R.id.humidity);
+        weekTv = (TextView) findViewById(R.id.week_today);
+        pmDataTv = (TextView) findViewById(R.id.pm_data);
+        pmQualityTv = (TextView) findViewById(R.id.pm2_5_quality);
+        pmImg = (ImageView) findViewById(R.id.pm2_5_img);
+        temperatureTv = (TextView) findViewById(R.id.temperature);
+        climateTv = (TextView) findViewById(R.id.climate);
+        windTv = (TextView) findViewById(R.id.wind);
+        weatherImg = (ImageView) findViewById(R.id.weather_img);
+
+        city_name_Tv.setText("N/A");
+        cityTv.setText("N/A");
+        timeTv.setText("N/A");
+        humidityTv.setText("N/A");
+        pmDataTv.setText("N/A");
+        pmQualityTv.setText("N/A");
+        weekTv.setText("N/A");
+        temperatureTv.setText("N/A");
+        climateTv.setText("N/A");
+        windTv.setText("N/A");
+    }
     /**
      * 获取cityCode所指的城市的天气信息
      * @param cityCode   城市id
@@ -107,6 +141,10 @@ public class MainActivity extends Activity implements View.OnClickListener{
         }).start();
     }
 
+    /**
+     * 解析XML
+     * @param xmldata
+     */
     private void parseXML(String xmldata){
         int fengxiangCount = 0;
         int fengliCount = 0;
